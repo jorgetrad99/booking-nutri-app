2ff15cab49acb22de6638ccb329d84792e4e32ed
Jorge Trad <jetradl@gmail.com>
2022-12-04 14:11:56 -0600

    Add functionality to Main Activity

--- app/src/main/java/com/example/citas_nutri/MainActivity.java
+++ app/src/main/java/com/example/citas_nutri/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.citas_nutri;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void irAIngresar(View view) {
+        Intent intent = new Intent(MainActivity.this, Ingresar.class);
+        startActivity(intent);
+    }
+
+    public void irARegistrar(View view) {
+        Intent intent = new Intent(MainActivity.this, Registrar.class);
+        startActivity(intent);
     }
 }
