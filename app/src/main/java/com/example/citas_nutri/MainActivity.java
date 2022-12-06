96730c330712f57f1358e7c84f5a8b8447bf3a18
Karen <karencanche930@gmail.com>
2022-12-06 13:49:22 -0600

    Diseño de interfaz Ingresa_Usua

--- app/src/main/java/com/example/citas_nutri/MainActivity.java
+++ app/src/main/java/com/example/citas_nutri/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/citas_nutri/Revisa_Nutri_Citas.java
@@ -0,0 +1,13 @@
+package com.example.citas_nutri;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Revisa_Nutri_Citas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_revisa_nutri_citas);
+    }
+}
